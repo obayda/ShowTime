dd4d5ee40a22d3138b07e839212c6bd235f3b149
obaydah <obayda7h@gmaill.com>
2019-06-22 18:43:57 +0300

    movie details page setup related #7

--- components/movieDetails/index.js
+++ components/movieDetails/index.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image
 } from "react-native";
 
 class MovieDeatils extends Component {
     render() {
         const { movie } = this.props;
+        let noImage = 'https://uae.microless.com/cdn/no_image.jpg';
         return (
-            <View style={styles.container}>
-                <Text>{movie.Title}</Text>
+            <View style={styles.actionBar}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>{movie.Title}</Text>
+                    <Image
+                        style={styles.image}
+                        source={movie.Poster == 'N/A' ? { uri: noImage } : { uri: movie.Poster }}
+                    />
+                    <View style={styles.textView}>
+                        <Text style={styles.textViewSize}>
+                            {movie.Released == 'N/A' ? "Release date wasn't provided" : movie.Released}
+                        </Text>
+                        <Text style={styles.textViewSize}>{movie.Genre}</Text>
+                        <Text style={styles.textViewSize}>{movie.Plot}</Text>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -21,6 +36,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: '#e1e5ed',
+    },
+    image: {
+        justifyContent: 'center',
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        marginTop: 10
+    },
+    title: {
+        marginTop: 20,
+        fontSize: 28,
+        textAlign: 'center'
+    },
+    textView: {
+        flex: 1,
+        alignItems: 'center',
+        width: 350,
+        marginTop: 20,
+    },
+    textViewSize: {
+        fontSize: 20
     }
 });
